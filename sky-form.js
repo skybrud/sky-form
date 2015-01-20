afa7b6f8819e0b221ab83178c7871aa88db39aaf
Filip Bruun Bech-Larsen <fbruun@skybrud.dk>
2015-01-20 11:44:24 +0100

    Adding the meta information to the actual file

--- sky-form.js
+++ sky-form.js
@@ -1,3 +1,12 @@
+/**
+ * skyform
+ * Enhancing the styleability of form-fields with AngularJS.
+ *
+ * @author Filip Bruun Bech-Larsen, @filipbech
+ * @link https://github.com/filipbech/skyform
+ * @license GNU GPL v2 License, https://raw.githubusercontent.com/filipbech/skyform/master/LICENSE
+ */
+
 /* global angular, window */
 (function () {
 	'use strict';
