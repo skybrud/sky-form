8499f65acebb92ed6529f1e2eba15fd8cb6376ae
Filip Bruun Bech-Larsen <fbruun@skybrud.dk>
2015-03-16 14:11:45 +0100

    Build-step implemented To build locally, run npm install and then grunt

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		uglify: {
+			toDist: {
+				options: {
+					preserveComments:false
+				},
+				files: [{
+					src: [
+						'*.module.js',
+						'*.provider.js',
+						'*.config.js',
+						'*.js',
+					],
+					dest: 'dist/skyform.min.js'
+				}]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('default', ['uglify:toDist']);
+};
